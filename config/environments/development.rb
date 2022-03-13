892eccbe0da5c95b71558564e561dcff7bcdf1ec
sangla3012 <lasang3012@gmail.com>
2022-03-13 17:08:34 +0700

    config with db of heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "ec2-34-194-25-190.compute-1.amazonaws.com"
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
