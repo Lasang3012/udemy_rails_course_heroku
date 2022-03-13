4f6e5d9d93f1598e3a6c04e65fb62fd83446f934
sangla3012 <lasang3012@gmail.com>
2022-03-13 18:28:47 +0700

    add font,bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import * as bootstrap from "bootstrap";
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
-require("trix")
-require("@rails/actiontext")
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap/dist/css/bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "@fortawesome/fontawesome-free/css/all";
+import "@fortawesome/fontawesome-free/js/all";
+
+require("trix");
+require("@rails/actiontext");
